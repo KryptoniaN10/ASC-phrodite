13d3c6f1cd54f2d24478bc4709917e86044fa6cd
KryptoniaN10 <rinjanlal13@gmail.com>
2026-08-15 11:11:36 +0530

    fps set to video fps

--- video_renderer.py
+++ video_renderer.py
@@ -1,12 +1,20 @@
 import cv2
 from PIL import Image
+import time
 from image_renderer import render_pillow_image
 
 def play_video(filename, screen):
     video = cv2.VideoCapture(filename)
+    fps=video.get(cv2.CAP_PROP_FPS)
+
+    if(fps<0):
+        print("could not determine video fps")
+        video.release()
+        return 
+    frame_duration=1/fps
 
     while True:
-
+        start=time.perf_counter()
         success, frame = video.read()
 
         if not success:
@@ -23,5 +31,9 @@ def play_video(filename, screen):
             pass
 
         render_pillow_image(img, screen)
+        elapsed=time.perf_counter()-start
+        remaining=frame_duration-elapsed
+        if(remaining>0):
+            time.sleep(remaining)
 
     video.release()
